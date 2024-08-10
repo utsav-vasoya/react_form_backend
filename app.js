544e78447ff808837d3a9987ec5b44aa9bdb2af8
Utsav Vasoya <utsavvasoya89@gmail.com>
2024-08-10 17:45:44 +0530

    changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 require('./config/passport')(passport);
 const db = require('./config/keys').mongoURL;
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
+    .then(() => console.log('MongoDB connected.'))
     .catch(err => console.log(err))
 app.use(expresslayout);
 app.use(express.json())
